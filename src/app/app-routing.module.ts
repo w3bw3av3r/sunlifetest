cb33253628f2ed7228b11c7d391c18298a67c405
Clem Borja <clem.borj30@gmail.com>
2020-04-05 09:57:05 +0800

    Remove home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	// {
-	// 	path: 'home',
-	// 	loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-	// },
 	{
 		path: 'dashboard',
 		loadChildren: () =>
